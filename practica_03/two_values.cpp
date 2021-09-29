0c3f7c1899f5bc2a6d4ac8c364043f9b6e64de95
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-09-29 07:49:28 -0500

    ordenar el vector para hallar la suma

--- practica_03/two_values.cpp
+++ practica_03/two_values.cpp
@@ -1,54 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void TwoValues(vector<int>& nums,int k){
-    bool isValues=false;
-    int mayor=0,m=0,p=0,q=nums.size()-1;
-    for(int i=0; i<nums.size();i++){
-        if(nums[i]>mayor && nums[i]<k){
-            mayor=nums[i];
-            m=i;
-        }
-    }
-
-    while(p<q){
-        if(p==m) p++;
-        if(q==m) q--;
-        
-        if(nums[p]+nums[m]==k){
-            if(p>m) cout<<m+1<<" "<<p+1<<"\n";
-            else cout<<p+1<<" "<<m+1<<"\n";
-            isValues=true;
-        }
-        if(nums[q]+nums[m]==k){
-            if(q>m) cout<<m+1<<" "<<q+1<<"\n";
-            else cout<<q+1<<" "<<m+1<<"\n";
-            isValues=true;
-        }
-        if(nums[p]+nums[q]==k){
-            if(p>q) cout<<q+1<<" "<<p+1<<"\n";
-            else cout<<p+1<<" "<<q+1<<"\n";
-            isValues=true;
-        }
-        if(nums[p]>nums[q]){
-            q--;
-        }
-        else p++;
-    }
-    if(!isValues) cout<<"IMPOSSIBLE\n";
-
+void TwoValues(vector<pair<int,int>>& nums,int k){
+    int i = 0, j = n - 1;
+	
+	while (i < j) {
+		// adjust left and right pointers.
+		if(nums[i].first + nums[j].first > k)
+			j--;
+		else if (nums[i].first + nums[j].first < k)
+			i++;
+		else if (nums[i].first + nums[j].first == k) {
+			cout << nums[i].second << " " << nums[j].second;
+			return;
+		}
+	}
+	cout << "IMPOSSIBLE" << endl;
 }
+
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<int> nums;
+    vector<pair<int,int>> nums; //implementamos un vector de pares
     int n=0,x=0,a=0;
     cin>>n>>x;
     for(int i=0;i<n;i++){
         cin>>a;
-        nums.push_back(a);
+        nums.push_back({a,i+1});
     }
+    sort(nums.begin(), nums.end()); //ordenamos el vector
     TwoValues(nums,x);
     return 0;
 }
