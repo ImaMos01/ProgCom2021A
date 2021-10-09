bf4ec15b4857078dd38443ed7c7012067f8e0b81
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-10-09 17:02:52 -0500

    Q7 Reversals and Sums

---
+++ practica_04_examen/q7_rever.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverSum(vector<long long int>& nums,long long int m){
+    vector<long long int> sums;
+    long long int n=nums.size(), opc=0,x=0,y=0;
+    sums.push_back(nums[0]);
+
+    for(long long int i=1;i<n;i++){
+        sums.push_back(nums[i]+sums[i-1]);
+    }
+
+    for(long long int i=0; i<m; i++){
+        cin>>opc>>x>>y;
+        if(opc==1){
+        }
+        else{
+            long long int sum=0;
+            if(x==y){
+            sum=nums[x-1];
+            }
+            else if(x>=2){
+                sum=sums[y-1]-sums[x-2];
+            }
+            else if(x==1){
+                sum=sums[y-1];
+            }
+            cout<<sum<<"\n";
+
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    vector<long long int> nums; 
+    long long int n=0,x=0,m=0;
+    cin>>n>>m;
+    for(long long int i=0;i<n;i++){
+        cin>>x;
+        nums.push_back(x);
+    }
+    reverSum(nums,m);
+ 
+    return 0;
+}
