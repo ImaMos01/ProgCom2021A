1322fa5d42cc815155fb1b87f284f206c1521311
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-10-09 19:31:18 -0500

    Q6 Palindrome Queries

--- practica_04_examen/q6_palin.cpp
+++ practica_04_examen/q6_palin.cpp
@@ -1,44 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void palindrome(vector<char>& name,long long int m){
-    long long int opc=0,x=0;
-    char y;
-    for(long long int i=0; i<m; i++){
-        cin>>opc>>x>>y;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    vector<char> name; 
+    long long int opc=0, n=0,m=0;
+    char x,d;
+    long long int a=0,b=0;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        name.push_back(x);
+    }
+
+    for(int i=0; i<m; i++){
+        cin>>opc;
         if(opc==1){
-            name[x-1]=y;
+            cin>>a>>d;
+            name[a-1]=d;
         }
         else{
-            int n=y-'0',f=1,mid=((n+x-1)/2),j=x-1;
-            while(j<=mid){
-                if(name[j]!=name[n-j+x-2]){
+            cin>>a>>b;
+            int p=a-1,q=b-1;
+            int mid=(p+q)/2,f=1;
+
+            while(p<=mid){
+                if(name[p]!=name[q]){
                     f=0;
-                    break;
+                break;
                 }
-                j++;
+                p++;
+                q--;
             }
             if(f==1){
                 cout<<"YES\n";
             }
             else{
                 cout<<"NO\n";
-            }
+            }   
         }
     }
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    vector<char> name; 
-    long long int n=0,m=0;
-    char x;
-    cin>>n>>m;
-    for(int i=0;i<n;i++){
-        cin>>x;
-        name.push_back(x);
-    }
-    palindrome(name,m);
     return 0;
 }
