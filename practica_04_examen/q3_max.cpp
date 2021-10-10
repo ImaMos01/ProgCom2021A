05e3dfa9bec0e72bb6b20d8b5d1027448a58551d
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-10-09 19:13:37 -0500

    Q3 Maximum Subarray Sum II

--- practica_04_examen/q3_max.cpp
+++ practica_04_examen/q3_max.cpp
@@ -1,39 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void maximumSub(vector<int>& nums, int a,int b){
-    //implementando el algoritmo de kadane
-    deque<int> subLenB;
-    deque<int> subLenA;
-    long long int s=nums.size(),localMaxB=0, globalMax=INT_MIN,lenB=b;
-    long long int lenA=a, localMaxA=0;
-
-    for(int i=0;i<s;i++){
-        subLenB.push_back(nums[i]);
-        subLenA.push_back(nums[i]);
-        
-        localMaxB=localMaxB+nums[i];
-        localMaxA=localMaxA+nums[i];
-        
-        if(subLenB.size()>=lenB){
-            if(globalMax<localMaxB) globalMax=localMaxB;
-            localMaxB-=subLenB.front();
-            subLenB.pop_front();
-        }
-        if(subLenA.size()>lenA){
-            if(globalMax<localMaxA) globalMax=localMaxA;
-            localMaxA-=subLenA.front();
-            subLenA.pop_front();
-        }
+ 
+void maximumSub(vector<long long>& nums, long long a,long long b){
+    set<pair<long long,int>> subArr;
+    vector<long long> sums;
+	sums.push_back(nums[0]);
+	long long sumMax=INT_MIN;
+	int s=nums.size();
+	for(int i=1;i<s;i++){
+		sums.push_back(nums[i]+sums[i-1]);
+	}
+ 
+    for(int i=(a-1);i<b;i++){
+        subArr.insert({sums[i],i});
     }
-    cout<<globalMax;
+	for(int i=0;i<s-a+1;i++){
+		sumMax = max(sumMax,subArr.rbegin()->first-sums[i-1]);
+		subArr.erase({sums[i+a-1],i+a-1});
+		if(i+b<s){
+			subArr.insert({sums[i+b],i+b});
+		}
+	}
+ 
+    cout<<sumMax;
 }
-
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<int> nums; 
-    long long int n=0,x=0,a=0,b=0;
+    vector<long long> nums; 
+    long long n=0,x=0,a=0,b=0;
     cin>>n>>a>>b;
     for(int i=0;i<n;i++){
         cin>>x;
