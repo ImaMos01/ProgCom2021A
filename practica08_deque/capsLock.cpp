772982945ce1f96611e76a10c09a4b6a45349dc6
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-10-26 14:34:29 -0500

    Ejercicio Caps Lock

--- practica08_deque/capsLock.cpp
+++ practica08_deque/capsLock.cpp
@@ -5,21 +5,32 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 	
-	queue<int> may;
-	string message;
+	queue<char> may;
+	string message="abc$d@ef$@g$";
+	string final="";
+	bool opc=false;
     
 	for(int i=0;i<message.length();i++){
 		if(message[i]=='@'){
-
+			if(opc==false){
+				opc=true;
+			}
+			else{ opc=false;}
 		}
 		else if(message[i]=='$'){
-
+			while(!may.empty()){
+				if(opc==true){
+					final+=toupper(may.front());
+				}
+				else{ final+=tolower(may.front());}
+				may.pop();
+			}
 		}
 		else{
 			may.push(message[i]);
 		}
 	}
-	
+	cout<<final;
 
     return 0;
 }
