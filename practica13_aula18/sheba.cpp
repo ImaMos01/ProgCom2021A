9c34483465cbe1c7d3d4b254360491c7dbe9207b
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-11-20 16:03:48 -0500

    Kattis. Sheba's Amoebas

---
+++ practica13_aula18/sheba.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs (vector<vector<char>>& image,int r1,int s1){
+    if(r1<0 || r1>=image.size() || s1<0 || s1>=image[r1].size() ||image[r1][s1]!='#'){
+        return;
+    }
+
+    image[r1][s1]='.';
+    dfs(image, r1+1, s1);
+    dfs(image, r1-1, s1);
+    dfs(image, r1, s1+1);
+    dfs(image, r1, s1-1);
+    dfs(image, r1+1, s1-1);
+    dfs(image, r1-1, s1-1);
+    dfs(image, r1+1, s1+1);
+    dfs(image, r1-1, s1+1);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int m=0,n=0;
+    char a;
+    cin>>m>>n;
+    vector<vector<char>>sheba;
+
+    for(int i=0;i<m;i++){
+        vector<char> op;
+        for(int j=0; j<n;j++){
+            cin>>a;
+            op.push_back(a);
+        }
+        sheba.push_back(op);
+    }
+    int cont=0;
+    for(int i=0;i<m;i++){
+        for(int j=0; j<n;j++){
+            if(sheba[i][j]=='#'){
+                dfs(sheba,i,j);
+                cont++;
+            }
+        }
+    }
+    cout<<cont<<"\n";
+}
