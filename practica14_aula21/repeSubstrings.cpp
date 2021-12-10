71107fa91548607e771b934966a80a201abcf74a
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-12-10 12:56:46 -0500

    Kattis. Repeated Substrings (update)

--- practica14_aula21/repeSubstrings.cpp
+++ practica14_aula21/repeSubstrings.cpp
@@ -1,62 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int repeatString(string arr){
-    int n=arr.length(),ans=0;
-    priority_queue<string,vector<string>,greater<string>> sor;
-    vector<int> lcp(n,0);
-    for(int i=0;i<n;i++){
-        sor.push(arr.substr(i));
+const long long int limi=1e5+5;
+int n=0,t=0,ans=0;
+string arr;
+int sa[limi],lcp[limi];
+int tmp[limi],pos[limi];
+
+bool compare(int i, int j){
+    if(pos[i]!=pos[j]) return (pos[i]<pos[j]);
+    i+=t;
+    j+=t;
+    if(i<n && j<n) return (pos[i]<pos[j]);
+    return (i>j);
+}
+
+void suffixArray(){
+    for(int i=0; i<n;i++){
+        sa[i]=i;
+        pos[i]=arr[i];
     }
-
-    int k=0,cont=0;
-    string sub="";
-    while(!sor.empty()){
-        if(k>=1){
-            cont=0;
-            int lp=sor.top().length(),mp=sub.length();
-            for(int i=0;i<lp;i++){
-                if(i>=mp){
-                    lcp[k-1]=cont;
-                    break;
-                }
-                if(sub[i]==sor.top()[i]){ 
-                    cont++;
-                }
-                else{
-                    lcp[k-1]=cont;
-                    break;
-                }
-
-            }
+    for(t=1;t<2*n+1;t*=2){
+        sort(sa,sa+n,compare);
+        for(int i=0; i<n-1;i++){
+            tmp[i+1]=tmp[i];
+            if(compare(sa[i],sa[i+1])) tmp[i+1]++;
         }
-        sub=sor.top();
-        sor.pop();
-        k++;
+        for(int i=0; i<n;i++){
+            pos[sa[i]]=tmp[i];
+        }
+        if(tmp[n-1]==n-1) break;
     }
+}
 
-    lcp[n-1]=0;
-    ans=lcp[0];
-    int uy=lcp.size();
-    for(int i=1; i<uy;i++){
-        if(lcp[i]>lcp[i-1]) {
-            ans+=(lcp[i]-lcp[i-1]);
+void find_lcp(){
+    int p=0;
+    for(int i=0; i<n;i++){
+        if(pos[i]!=n-1){
+            int j=sa[pos[i]+1];
+            while(arr[i+p]==arr[j+p])p++;
+            lcp[pos[i]]=p;
+            if(p>0) p--;
         }
     }
-    
-    return ans;
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int x=0;
-    string arr;
     cin>>x;
-    cin.ignore();
+    
     for(int i=0;i<x;i++){
-        getline(cin, arr);
-        cout<<repeatString(arr)<<"\n";   
+        cin>>arr;
+        n=arr.length();
+        
+        ans=lcp[0];
+        for(int j=1; j<n;j++){
+            if(lcp[j]>lcp[j-1]) {
+                ans+=(lcp[j]-lcp[j-1]);
+            }
+        }
+        cout<<ans<<"\n";
     }
     return 0;
 }
