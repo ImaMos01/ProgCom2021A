54e78a7e363ea975499d8931bb191b685aaf7910
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-12-10 13:49:15 -0500

    Beecrowd. Substring Distribution(update)

--- practica15_aula22/manySubs.cpp
+++ practica15_aula22/manySubs.cpp
@@ -1,63 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int uniqueSub(string arr){
-    if(arr.empty()) return 0;
-    int n=arr.length();
-    map<string,int> sor;
-    vector<int> lcp(n,0);
-    string sub="";
-    int sumtot=(n*(n+1))/2;
-    for(int i=n-1;i>=0;i--){
-        sub=arr[i]+sub;
-        sor[sub]=i;
+const long long int limi=2e5+5;
+int n=0,t=0,ans=0;
+string arr;
+int sa[limi],lcp[limi];
+int tmp[limi],pos[limi];
+
+bool compare(int i, int j){
+    if(pos[i]!=pos[j]) return (pos[i]<pos[j]);
+    i+=t;
+    j+=t;
+    if(i<n && j<n) return (pos[i]<pos[j]);
+    return (i>j);
+}
+
+void suffixArray(){
+    for(int i=0; i<n;i++){
+        sa[i]=i;
+        pos[i]=arr[i];
     }
-
-    int k=0,cont=0;
-    lcp[0]=0;
-    string prev;
-    for(auto j: sor){
-        if(k>=1){
-            cont=0;
-            string temp=j.first;
-            for(int i=0;i<temp.length();i++){
-                if(i>=prev.length()){
-                    lcp[k]=lcp[k-1]+cont;
-                    break;
-                }
-                if(prev[i]==temp[i]){ 
-                    cont++;
-                }
-                else{
-                    lcp[k]=lcp[k-1]+cont;
-                    break;
-                }
-
-            }
+    for(t=1;t<2*n+1;t*=2){
+        sort(sa,sa+n,compare);
+        for(int i=0; i<n-1;i++){
+            tmp[i+1]=tmp[i];
+            if(compare(sa[i],sa[i+1])) tmp[i+1]++;
         }
-        prev=j.first;
-        k++;
+        for(int i=0; i<n;i++){
+            pos[sa[i]]=tmp[i];
+        }
+        if(tmp[n-1]==n-1) break;
     }
-    
-    return sumtot-lcp[n-1];
+}
+
+void find_lcp(){
+    int p=0;
+    int sumtot=(n*(n+1))/2;
+    for(int i=0; i<n;i++){
+        if(pos[i]!=n-1){
+            int j=sa[pos[i]+1];
+            while(arr[i+p]==arr[j+p])p++;
+            lcp[pos[i]]=p;
+            ans+=p;
+            if(p>0) p--;
+        }
+    }
+    cout<<sumtot-ans<<"\n";
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int cont=0,aux=0;
-    string arr;
-    while(cin>>arr){
-        string temp="";
-        int n=arr.length();
-        for(int i=0; i<n;i++){
-            if(arr[i]=='?'){
-                cout<<uniqueSub(temp)<<"\n";
+    int x=0;
+    string temp;
+    while(cin>>temp){
+        if(temp=="0") break;
+        int k=temp.length();
+        for(int i=0; i<k;i++){
+            if(temp[i]=='?'){
+                suffixArray();
+                find_lcp();
             }
             else{
-                temp+=arr[i];
+                arr+=temp[i];
+                n++;
             }
         }
+        n=0;
+        arr.clear();
     }
     return 0;
 }
