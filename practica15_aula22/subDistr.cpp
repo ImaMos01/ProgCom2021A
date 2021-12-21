b3524e0ed7d64925d2c1ec982fcec18f0a59cb6f
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2021-12-20 20:03:44 -0500

    Cses. Substring Distribution (update)

--- practica15_aula22/subDistr.cpp
+++ practica15_aula22/subDistr.cpp
@@ -1,60 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int uniqueSub(string arr){
-    int n=arr.length();
-    map<string,int> sor;
-    vector<int> lcp(n,0);
-    string sub="";
-    int sumtot=(n*(n+1))/2;
-    for(int i=n-1;i>=0;i--){
-        sub=arr[i]+sub;
-        sor[sub]=i;
-    }
+const long long int limi=1e5+5;
+int n=0,t=0;
+string arr;
+int sa[limi],lcp[limi],opc[limi];
+int tmp[limi],pos[limi];
 
-    int k=0,cont=0;
-    lcp[0]=0;
-    string prev;
-    for(auto j: sor){
-        if(k>=1){
-            cont=0;
-            string temp=j.first;
-            for(int i=0;i<temp.length();i++){
-                if(i>=prev.length()){
-                    lcp[k]=lcp[k-1]+cont;
-                    break;
-                }
-                if(prev[i]==temp[i]){ 
-                    cont++;
-                }
-                else{
-                    lcp[k]=lcp[k-1]+cont;
-                    break;
-                }
-
-            }
-        }
-        prev=j.first;
-        k++;
-    }
-    
-    return sumtot-lcp[n-1];
+bool compare(int i, int j){
+    if(pos[i]!=pos[j]) return (pos[i]<pos[j]);
+    i+=t;
+    j+=t;
+    if(i<n && j<n) return (pos[i]<pos[j]);
+    return (i>j);
 }
+
+void suffixArray(){
+    for(int i=0; i<n;i++){
+        sa[i]=i;
+        pos[i]=arr[i];
+    }
+    for(t=1;t<2*n+1;t*=2){
+        sort(sa,sa+n,compare);
+        for(int i=0; i<n-1;i++){
+            tmp[i+1]=tmp[i];
+            if(compare(sa[i],sa[i+1])) tmp[i+1]++;
+        }
+        for(int i=0; i<n;i++){
+            pos[sa[i]]=tmp[i];
+        }
+        if(tmp[n-1]==n-1) break;
+    }
+}
+
+void find_lcp(){
+    int p=0;
+    for(int i=0; i<n;i++){
+        if(pos[i]!=n-1){
+            int j=sa[pos[i]+1];
+            while(arr[i+p]==arr[j+p])p++;
+            lcp[pos[i]]=p;
+            if(p>0) p--;
+        }
+    }
+}
+
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int cont=0,aux=0;
-    string arr;
     cin>>arr;
-    int n=arr.length();
-    cont=uniqueSub(arr);
-    for(int i=1;i<n;i++){
-        aux=uniqueSub(arr.substr(i));
-        cout<<cont-aux<<" ";
-        cont=aux;
+    n=arr.length();
+    suffixArray();
+    find_lcp();
+    int q=0;
+    for(int i=0;i<n;i++){
+        opc[q+1]++;
+        opc[n-sa[i]+1]--;
+        q=lcp[i];
     }
-    cout<<1<<"\n";
+    for(int i=1; i<=n;i++){
+        cout<<opc[i]<<" ";
+        opc[i+1]+=opc[i];
+    }
 
     return 0;
 }
